15bb8664505424ff8f946070161c602ec62ffc28
Syr7-s <isasayar7@github.com>
2021-04-21 17:28:38 +0300

    Caesar named interface was implemented to the CaesarEncryption

--- src/com/syrisa/caesarencrypt/CaesarEncryption.java
+++ src/com/syrisa/caesarencrypt/CaesarEncryption.java
@@ -1,7 +1,17 @@
 package com.syrisa.caesarencrypt;
 
-public class CaesarEncryption {
+public class CaesarEncryption implements Caesar{
     private int sentenceLength;
     private char[] turkishAlphabet = {'ğ', 'ı', 'ü', 'ö', 'ç', 'ş'};
     private char[] latinAlphabet = {'g', 'i', 'u', 'u', 'o', 'c', 's'};
+
+    @Override
+    public String unnecessaryCharacterRemove(String sentence) {
+        return null;
+    }
+
+    @Override
+    public String convertTurkishAlphabetsToLatinAlphabet(String message) {
+        return null;
+    }
 }
