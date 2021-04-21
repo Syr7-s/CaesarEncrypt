28f2c0995722c91fbed737f1e8d7f9bdc5da6539
Syr7-s <isasayar7@github.com>
2021-04-21 17:35:19 +0300

    unnecessaryCharacterRemove method content was filled.

--- src/com/syrisa/caesarencrypt/CaesarEncryption.java
+++ src/com/syrisa/caesarencrypt/CaesarEncryption.java
@@ -1,13 +1,15 @@
 package com.syrisa.caesarencrypt;
 
-public class CaesarEncryption implements Caesar{
+import java.util.Locale;
+
+public class CaesarEncryption implements Caesar {
     private int sentenceLength;
     private char[] turkishAlphabet = {'ğ', 'ı', 'ü', 'ö', 'ç', 'ş'};
     private char[] latinAlphabet = {'g', 'i', 'u', 'u', 'o', 'c', 's'};
 
     @Override
     public String unnecessaryCharacterRemove(String sentence) {
-        return null;
+        return sentence.toLowerCase().replaceAll("['a-z]", "");
     }
 
     @Override
