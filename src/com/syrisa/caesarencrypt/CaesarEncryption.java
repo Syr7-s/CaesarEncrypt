b33d595434b95e3fa31b1ae9620b2a5a515c745f
Syr7-s <isasayar7@github.com>
2021-04-21 17:27:40 +0300

    turkishAlphabet and latinAlphabet char array were defined.

--- src/com/syrisa/caesarencrypt/CaesarEncryption.java
+++ src/com/syrisa/caesarencrypt/CaesarEncryption.java
@@ -2,4 +2,6 @@ package com.syrisa.caesarencrypt;
 
 public class CaesarEncryption {
     private int sentenceLength;
+    private char[] turkishAlphabet = {'ğ', 'ı', 'ü', 'ö', 'ç', 'ş'};
+    private char[] latinAlphabet = {'g', 'i', 'u', 'u', 'o', 'c', 's'};
 }
