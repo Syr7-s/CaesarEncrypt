168fe5be173eb26d8121e6d7069752e553819861
Syr7-s <isasayar7@github.com>
2021-04-21 17:11:47 +0300

    default access modifier String return type message named method was created.

--- src/com/syrisa/caesarencrypt/Caesar.java
+++ src/com/syrisa/caesarencrypt/Caesar.java
@@ -1,4 +1,9 @@
 package com.syrisa.caesarencrypt;
 
+import java.util.Scanner;
+
 public interface Caesar {
+    default String message(){
+        return new Scanner(System.in).nextLine();
+    }
 }
